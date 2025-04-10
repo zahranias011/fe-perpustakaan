19b676eeadf0103054ddad5dd06ec572645a5917
zahrani <zahranias13@gmail.com>
2025-04-10 12:04:12 +0700

    Update file sesuai perubahan terbaru

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import signin from '../views/signin.vue';
 import register from '../views/register.vue';
 import beranda from '../views/beranda.vue';
 import tentangkami from '../views/tentangkami.vue';
+import Kategori from '../views/kategori.vue';
 
 const routes = [
   { path: '/signin', name: 'SignIn', component: signin },
   { path: '/register', name: 'Register', component: register },
   { path: '/', name: 'Beranda', component: beranda},
   { path: '/tentangkami', name: 'TentangKami', component: tentangkami},
+  { path: '/kategori', name: 'Kategori', component: Kategori},
 ];
 
 const router = createRouter({
